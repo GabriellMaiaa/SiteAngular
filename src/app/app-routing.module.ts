26fef14db004bb2829f43e7ceecd4ece9dd59391
Gabriel Maia <gribeiromaia736@gmail.com>
2022-10-20 22:59:04 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { CategoriaComponent } from './pages/categoria/categoria.component';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+
+const routes: Routes = [
+  {path: 'categoria', component:CategoriaComponent},
+  {path: '',component: HomeComponent}, 
+  {path: 'menu',component: MenuComponent},
+  {path:'about', component:AboutComponent}  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
